115e3199ae360c428d94c49700c36fb37f4565fe
zhiminghzou <346400498@qq.com>
2018-07-31 14:20:53 +0800

    提交

---
+++ rxjavatext/src/main/java/com/test/rxjavatext/MainActivity.java
@@ -0,0 +1,26 @@
+package com.test.rxjavatext;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.functions.Action1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] names = {"Hi","Hello","Mack"};
+        Observable.from(names)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String name) {
+                        Log.i("MainActivity",name);
+                    }
+                });
+
+    }
+}
